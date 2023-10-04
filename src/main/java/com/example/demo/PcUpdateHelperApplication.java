56498a730efb4a12e5c2aab1178018c946870872
Camille <chauenov2023@gmail.com>
2023-10-04 19:56:27 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/PcUpdateHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcUpdateHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcUpdateHelperApplication.class, args);
+	}
+
+}
