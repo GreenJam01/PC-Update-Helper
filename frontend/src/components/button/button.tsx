a2962d6d67530d8eaa761af48e69818b22c753ca
Camille <KCHauenov@IBS.RU>
2023-12-30 16:29:31 +0400

    Tried to implement post request on frontend.

--- frontend/src/components/button/button.tsx
+++ frontend/src/components/button/button.tsx
@@ -2,25 +2,36 @@ import React from "react";
 import "./button.css"
 
 export function Button<T>(
-    {children, className, type = "primary", filling = undefined, clickListener = ()=>{}, onFocus = ()=>{}, onBlur = ()=>{}}: {
-        children: React.ReactNode,
-        className?: string,
-        type?: string,
-        filling?: T,
-        clickListener?: (e:  React.MouseEvent<HTMLButtonElement, MouseEvent>, t:T) => void,
-        onFocus?: ()=>void,
-        onBlur?: ()=>void
+    {
+      children,
+      className,
+      type = "primary",
+      filling = undefined,
+      clickListener = (e, t) => {},
+      onFocus = () => {},
+      onBlur = () => {},
+      onClick = () => {} // Добавим свойство onClick в параметры компонента
+    }: {
+      children: React.ReactNode,
+      className?: string,
+      type?: string,
+      filling?: T,
+      clickListener?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, t: T) => void,
+      onFocus?: () => void,
+      onBlur?: () => void,
+      onClick?: () => void // Обновленное свойство onClick
     }
-){
+  ){
     let buttonType = "button--" + type + " ";
     return(
-        <button
-            className={"button "+ buttonType + className!}
-            onClick={(event)=>{clickListener(event, filling!)}}
-            onFocus={()=>{onFocus()}}
-            onBlur={()=>{onBlur()}}>
-
-            {children}
-        </button>
+      <button
+          className={"button "+ buttonType + (className ? className : "")}
+        //   onClick={(event)=>{clickListener(event, filling!)}}
+          onFocus={()=>{onFocus()}}
+          onBlur={()=>{onBlur()}}
+          onClick={onClick} // Передаем свойство onClick внутреннему элементу button
+      >
+          {children}
+      </button>
     )
-}
+  }
--- frontend/src/pages/create/CreatePage.tsx
+++ frontend/src/pages/create/CreatePage.tsx
@@ -3,15 +3,55 @@ import {Container} from "../../components/container/container";
 import {SelectList} from "../../components/select-list/select-list";
 import {hdd, motherboards, processors, videoCards, RAMs, SSD} from "../../data/appointment-list";
 import {Button} from "../../components/button/button";
+import {useEffect, useState} from "react";
+import {Assembly} from "C:/Users/KChauenov/PC-Update-Helper/frontend/src/data/assembly_dto"
 
-export function CreatePage(){
+    export function CreatePage() {
+        const initialFormData: Assembly = {
+            cpu: "lj",
+            gpu: "jl",
+            ram: "ewd",
+            motherboard: "rqwe",
+            hdd: "rqew"
+          };
+        const [formData, setFormData] = useState<Assembly>(initialFormData);
+
+        const handleFormSubmit = async () => {
+            try {
+              const response = await fetch("http://localhost:8080/assemblies", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+              });
+        
+              if (response.ok) {
+                const data = await response.json();
+                // Обработка успешного ответа
+              } else {
+                // Обработка ошибки
+                throw new Error("Ошибка HTTP: " + response.status);
+              }
+            } catch (error) {
+              // Обработка сбоя запроса
+              console.error("Ошибка при отправке запроса:", error);
+            }
+          };
+        
+          const handleInputChange = (field: any, value: any) => {
+            setFormData((prevData) => ({
+              ...prevData,
+              [field]: value
+            }));
+          };
     return(
         <>
-        <div className={"a12 xs12 s12 center"}><h1>Что-то про ПК</h1></div>
+        <div className={"a12 xs12 s12 center"}><h1>PC Update Helper</h1></div>
             <Container className={"assemble_page__container"}>
                 <div className={"assemble_page__section a6 xs12 s12"}>
                     <h1>Процессор:</h1>
-                    <SelectList options={processors}/>
+                    <SelectList  options={processors} />
                     <h1>Видеокарта:</h1>
                     <SelectList options={videoCards}/>
                     <h1>HDD:</h1>
@@ -27,7 +67,7 @@ export function CreatePage(){
                 </div>
             </Container>
             <div className={"isolated center"}>
-                <Button>Собрать</Button>
+                <Button onClick={handleFormSubmit}>Собрать</Button>
             </div>
             <div className={"isolated center"}>
                 <LinkButton href={"/app/"}>На главную</LinkButton>
