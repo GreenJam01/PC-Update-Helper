d54458341aa21b658d7b3996707cc83f4ffd509e
Techies228 <tlmiracle@yandex.ru>
2024-01-02 00:13:41 +0400

    an unfinished parser

---
+++ parser/Parser/spiders/motherboardParser.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+
+class MotherboardparserSpider(scrapy.Spider):
+    name = "motherboardParser"
+    allowed_domains = ["citilink.ru"]
+  # start_urls = ["https://citilink.ru/catalog/materinskie-platy"]
+
+    def start_requests(self):
+        url = "https://www.citilink.ru/catalog/materinskie-platy/"
+        yield SeleniumRequest( url=url,
+                               callback=self.parse )
+
+    def parse(self, response): 
+        for motherboard in response.css("div.app-catalog-1tp0ino.e1an64qs0 a"):
+            yield {
+                    "motherboard": motherboard.attrib["title"].split(",")[0] 
+                  }
