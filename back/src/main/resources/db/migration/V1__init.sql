3d4d6599ac8fce2cfbe16b18158e0ae5b87bbd06
Camille <KCHauenov@IBS.RU>
2024-02-25 01:01:50 +0400

    Added an authentication

---
+++ back/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,4 @@
+insert into users(email, password,username) values("admin@mail.ru", "123456", "admin")
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
