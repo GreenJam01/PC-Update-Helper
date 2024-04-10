b3db853ed70f3f3779b279d4b1bee5ca29ecf47d
Camille <KCHauenov@IBS.RU>
2024-04-11 02:05:46 +0400

    Добавил спиннер

--- frontend/src/components/hardwareList/HardwareList.tsx
+++ frontend/src/components/hardwareList/HardwareList.tsx
@@ -1,14 +1,20 @@
+import { BeatLoader } from 'react-spinners';
+import { useAppSelector } from '../../hooks/use-app';
+import { hardwaresSelectors } from '../../slices/hardwareSlice';
 import { HardwareType } from '../../types/hardwares';
 import { HardwareCard } from '../hardwareCard/HardwareCard';
 
 type HardwareListProps = {hardwares?: HardwareType[]; type:string}
 export function HardwareList(props: HardwareListProps){
+  const isHardwareDataLoading = useAppSelector(hardwaresSelectors.isHardwareDataLoading);
   const cards = props.hardwares?.map((i) =>
     (<HardwareCard key={i.id} hardware={i} type={props.type}/>));
   return (
-    <div className='hardware-list'>
-      {cards}
-    </div>
+    <section className='hardware-listWrapper'>
+      <div className='hardware-list'>
+        {isHardwareDataLoading ? <BeatLoader color="#36d7b7" /> : cards}
+      </div>
+    </section>
   );
 
 }
