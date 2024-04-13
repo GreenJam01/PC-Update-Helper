12a2a1162dfd71cac3998a34694759c410fec741
Camille <KCHauenov@IBS.RU>
2024-04-13 13:47:15 +0400

    Added checking for a role User

--- back/src/main/java/com/ezyxip/pcmback/controllers/MainRestController.java
+++ back/src/main/java/com/ezyxip/pcmback/controllers/MainRestController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -46,7 +47,7 @@ public class MainRestController {
     @Operation(
             summary = "Добавить сборку в базу данных"
     )
-
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     @PostMapping(value = "/assemblies",consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<AssemblyEntity> createAssembly(@RequestBody AssemblyEntity assembly) {
         try {
@@ -69,6 +70,7 @@ public class MainRestController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     @Operation(
             summary = "Получить список сборок из таблицы"
     )
