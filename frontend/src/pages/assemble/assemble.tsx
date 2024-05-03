ee9f3d1d57d3f126e0a00c55981395b0c60f3776
Techies228 <tlmiracle@yandex.ru>
2024-05-04 00:57:42 +0400

    мелкие изменения

--- frontend/src/pages/assemble/assemble.tsx
+++ frontend/src/pages/assemble/assemble.tsx
@@ -8,17 +8,22 @@ import {Button} from '../../components/button/button';
 import { AppRoutes } from '../../constants';
 import { Header } from '../../components/header/header';
 
+import './assemble.css'
 export function AssemblePage(){
   return(
     <>
       <Header/>
-      <div className={'a12 xs12 s12 center'}><h1>Улучшить ПК</h1></div>
+      <div className={'a12 xs12 s12 center'}>
+        <h1 className='assemble_page_text'>
+          Улучшить ПК
+        </h1>
+      </div>
       <Container className={'assemble_page__container'}>
         <div className={'assemble_page__section a6 xs12 s12'}>
-          <h1>Цена:</h1>
+          <h1 className='assemble_component_text'>Цена:</h1>
           <PriceInput className={'wide'} label={'От:'}/>
           <PriceInput className={'wide'} label={'До:'}/>
-          <h1>Процессор:</h1>
+          <h1 className='assemble_component_text'>Процессор:</h1>
           <div className={'assemble_page__radio-button'}>
             <input name={'1'} id={'radio-AMD'} type={'radio'}/>
             <label htmlFor={'radio-AMD'} radioGroup={'1'}>AMD</label>
@@ -29,16 +34,18 @@ export function AssemblePage(){
           </div>
         </div>
         <div className={'assemble_page__section a6 xs12 s12'}>
-          <h1>Назначение</h1>
+          <h1 className='assemble_component_text'>Назначение</h1>
           <SelectList options={appointmentList}/>
         </div>
+        <div>
+          <div className={'isolated center'}>
+            <Button>Собрать</Button>
+          </div>
+          <div className = {'isolated center'}>
+            <LinkButton href={AppRoutes.Main}>На главную</LinkButton>
+          </div>
+        </div>
       </Container>
-      <div className={'isolated center'}>
-        <Button>Собрать</Button>
-      </div>
-      <div className = {'isolated center'}>
-        <LinkButton href={AppRoutes.Main}>На главную</LinkButton>
-      </div>
     </>
   );
 }
--- frontend/src/pages/my-assemblies/MyAssembliesPage.tsx
+++ frontend/src/pages/my-assemblies/MyAssembliesPage.tsx
@@ -4,14 +4,18 @@ import { AssembliesSelectors } from '../../slices/assembliesSlice';
 import { AssemblyCard } from '../../components/assembly-card/assembly-card';
 import { Header } from '../../components/header/header';
 
-
+import "./MyAssembliesPage.css"
 export function MyAssembliesPage(){
   const assemblies = useAppSelector(AssembliesSelectors.assemblies);
   return(
     <>
       <Header/>
       <Container>
-        <div className={'a12 xs12 s12 center'}><h1>Мои сборки</h1></div>
+        <div className={'a12 xs12 s12 center'}>
+          <h1 className='myAssemblies-text'>
+            Мои сборки
+          </h1>
+        </div>
         <div className="skipX3"></div>
         <div className={'ao xs12 s12 center myAssembly'}>
           {assemblies.map((assembly) => (
